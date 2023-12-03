53338392d88974f67a1af1585def9f81ab851d53
Sander in 't Hout <sander@inthout.me>
2023-12-03 02:35:50 +0100

    Implement go_router for navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:collection/collection.dart';
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:go_router/go_router.dart';
 import 'package:h3x_devtools/solvers/advent_of_code/2021/_all_solvers.dart' as aoc2021;
 import 'package:h3x_devtools/solvers/advent_of_code/2022/_all_solvers.dart' as aoc2022;
 import 'package:h3x_devtools/solvers/advent_of_code/2023/_all_solvers.dart' as aoc2023;
@@ -9,6 +11,8 @@ import 'package:h3x_devtools/views/challenge_solver_view.dart';
 import 'package:h3x_devtools/views/overlay_effects.dart';
 import 'package:intl/intl.dart';
 import 'package:syntax_highlight/syntax_highlight.dart';
+
+part 'main.panel_items.dart';
 
 void main() async {
   // add OFL for Inconsolata font
@@ -35,110 +39,46 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-  static final NumberFormat _dayNumberFormat = NumberFormat('00');
-  int _topIndex = 0;
-  List<NavigationPaneItem>? _rootPainItems;
-  
   @override
   Widget build(BuildContext context) {
-    return FluentApp(
+    return FluentApp.router(
       title: 'Coding Challenges Tool',
       darkTheme: FluentThemeData.dark(),
-      home: NavigationView(
+      routerConfig: _router,
+    );
+  }
+
+}
+
+int _calculateSelectedIndex(BuildContext context) {
+  final location = GoRouterState.of(context).uri.toString();
+  return _routes.indexWhere((route) => route.path == location);
+}
+
+final _rootNavigatorKey = GlobalKey<NavigatorState>();
+final _shellNavigatorKey = GlobalKey<NavigatorState>();
+final _router = GoRouter(navigatorKey: _rootNavigatorKey, routes: [
+  ShellRoute(
+    navigatorKey: _shellNavigatorKey,
+    routes: _routes,
+    builder: (context, state, child) {
+      return NavigationView(
         appBar: const NavigationAppBar(
           title: Text('h3x4d3c1m4l\'s Coding Challenges Tool'),
           automaticallyImplyLeading: false,
         ),
         pane: NavigationPane(
-          selected: _topIndex,
-          onChanged: (index) => setState(() {
-            _topIndex = index;
-          }),
-          items: _getPaneItems(),
+          selected: _calculateSelectedIndex(context),
+          items: _rootPainItems,
         ),
-      ),
-    );
-  }
-
-  List<NavigationPaneItem> _getPaneItems() {
-    return _rootPainItems ??= [
-      PaneItem(
-        icon: const Icon(FluentIcons.home),
-        title: const Text('Home'),
-        body: const Center(
-          child: Text("Welcome!"),
-        ),
-      ),
-      _adventOfCode2021PaneItem,
-      _adventOfCode2022PaneItem,
-      _adventOfCode2023PaneItem,
-      _overlayEffectsItem,
-    ];
-  }
-
-  PaneItemExpander get _adventOfCode2021PaneItem {
-    return PaneItemExpander(
-      icon: const Icon(FluentIcons.code),
-      title: const Text('Advent of Code 2021'),
-      body: const Center(child: Text("Choose a day from the sub menu")),
-      items: [
-        _getAdventOfCodePaneItem(2021, 01, aoc2021.Day01Solver()),
-        _getAdventOfCodePaneItem(2021, 02, aoc2021.Day02Solver()),
-      ],
-    );
-  }
-
-  PaneItemExpander get _adventOfCode2022PaneItem {
-    return PaneItemExpander(
-      icon: const Icon(FluentIcons.code),
-      title: const Text('Advent of Code 2022'),
-      body: const Center(child: Text("Choose a day from the sub menu")),
-      items: [
-        _getAdventOfCodePaneItem(2022, 01, aoc2022.Day01Solver()),
-        _getAdventOfCodePaneItem(2022, 02, aoc2022.Day02Solver()),
-        _getAdventOfCodePaneItem(2022, 03, aoc2022.Day03Solver()),
-        _getAdventOfCodePaneItem(2022, 04, aoc2022.Day04Solver()),
-        _getAdventOfCodePaneItem(2022, 05, aoc2022.Day05Solver()),
-        _getAdventOfCodePaneItem(2022, 06, aoc2022.Day06Solver()),
-        _getAdventOfCodePaneItem(2022, 07, aoc2022.Day07Solver()),
-        _getAdventOfCodePaneItem(2022, 08, aoc2022.Day08Solver()),
-        _getAdventOfCodePaneItem(2022, 09, aoc2022.Day09Solver()),
-        _getAdventOfCodePaneItem(2022, 10, aoc2022.Day10Solver()),
-        _getAdventOfCodePaneItem(2022, 13, aoc2022.Day13Solver()),
-        _getAdventOfCodePaneItem(2022, 14, aoc2022.Day14Solver()),
-      ],
-    );
-  }
-
-  PaneItemExpander get _adventOfCode2023PaneItem {
-    return PaneItemExpander(
-      icon: const Icon(FluentIcons.code),
-      title: const Text('Advent of Code 2023'),
-      body: const Center(child: Text("Choose a day from the sub menu")),
-      items: [
-        _getAdventOfCodePaneItem(2023, 01, aoc2023.Day01Solver()),
-        _getAdventOfCodePaneItem(2023, 02, aoc2023.Day02Solver()),
-      ],
-    );
-  }
-
-  PaneItem _getAdventOfCodePaneItem(int year, int day, Solver solver) {
-    String dayString = _dayNumberFormat.format(day);
-
-    return PaneItem(
-      icon: const Icon(FluentIcons.issue_solid),
-      title: Text('Day $dayString'),
-      body: ChallengeSolverView(title: "AoC $year - Day $dayString", solver: solver),
-    );
-  }
-
-
-  PaneItem get _overlayEffectsItem {
-    return PaneItem(
-      icon: const Icon(FluentIcons.code),
-      title: const Text('Overlay effects'),
-      body: const Center(child: OverlayEffects()),
-    );
-  }
-
-}
+        paneBodyBuilder: (item, _) {
+          final name = item?.key is ValueKey ? (item!.key! as ValueKey).value : null;
+          return FocusTraversalGroup(
+            key: ValueKey('body$name'),
+            child: child,
+          );
+        },
+      );
+    },
+  ),
+]);
---
+++ lib/main.panel_items.dart
@@ -0,0 +1,146 @@
+part of 'main.dart';
+
+final NumberFormat _dayNumberFormat = NumberFormat('00');
+
+final List<NavigationPaneItem> _rootPainItems = [
+  _RoutingPaneItem(
+    routePath: '/',
+    icon: const Icon(FluentIcons.home),
+    title: const Text('Home'),
+    routeBody: const Center(
+      child: Text("Welcome!"),
+    ),
+  ),
+  _adventOfCode2021PaneItem,
+  _adventOfCode2022PaneItem,
+  _adventOfCode2023PaneItem,
+  _overlayEffectsItem,
+];
+
+final List<GoRoute> _routes = _rootPainItems.map((item) => switch (item) {
+    _RoutingPaneItem() => [item.goRoute],
+    _RoutingPaneItemExpander() => [item.goRoute, ...item.itemGoRoutes],
+    _ => throw Exception('Cannot convert unsupported type <${item.runtimeType}> to GoRoute')
+  }).flattened.toList();
+
+_RoutingPaneItemExpander get _adventOfCode2021PaneItem {
+  return _RoutingPaneItemExpander(
+    routePath: '/aoc2021',
+    icon: const Icon(FluentIcons.code),
+    title: const Text('Advent of Code 2021'),
+    routeBody: const Center(child: Text("Choose a day from the sub menu")),
+    items: [
+      _getAdventOfCodePaneItem(2021, 01, aoc2021.Day01Solver()),
+      _getAdventOfCodePaneItem(2021, 02, aoc2021.Day02Solver()),
+    ],
+  );
+}
+
+_RoutingPaneItemExpander get _adventOfCode2022PaneItem {
+  return _RoutingPaneItemExpander(
+    routePath: '/aoc2022',
+    icon: const Icon(FluentIcons.code),
+    title: const Text('Advent of Code 2022'),
+    routeBody: const Center(child: Text("Choose a day from the sub menu")),
+    items: [
+      _getAdventOfCodePaneItem(2022, 01, aoc2022.Day01Solver()),
+      _getAdventOfCodePaneItem(2022, 02, aoc2022.Day02Solver()),
+      _getAdventOfCodePaneItem(2022, 03, aoc2022.Day03Solver()),
+      _getAdventOfCodePaneItem(2022, 04, aoc2022.Day04Solver()),
+      _getAdventOfCodePaneItem(2022, 05, aoc2022.Day05Solver()),
+      _getAdventOfCodePaneItem(2022, 06, aoc2022.Day06Solver()),
+      _getAdventOfCodePaneItem(2022, 07, aoc2022.Day07Solver()),
+      _getAdventOfCodePaneItem(2022, 08, aoc2022.Day08Solver()),
+      _getAdventOfCodePaneItem(2022, 09, aoc2022.Day09Solver()),
+      _getAdventOfCodePaneItem(2022, 10, aoc2022.Day10Solver()),
+      _getAdventOfCodePaneItem(2022, 13, aoc2022.Day13Solver()),
+      _getAdventOfCodePaneItem(2022, 14, aoc2022.Day14Solver()),
+    ],
+  );
+}
+
+_RoutingPaneItemExpander get _adventOfCode2023PaneItem {
+  return _RoutingPaneItemExpander(
+    routePath: '/aoc2023',
+    icon: const Icon(FluentIcons.code),
+    title: const Text('Advent of Code 2023'),
+    routeBody: const Center(child: Text("Choose a day from the sub menu")),
+    items: [
+      _getAdventOfCodePaneItem(2023, 01, aoc2023.Day01Solver()),
+      _getAdventOfCodePaneItem(2023, 02, aoc2023.Day02Solver()),
+    ],
+  );
+}
+
+_RoutingPaneItem _getAdventOfCodePaneItem(int year, int day, Solver solver) {
+  String dayString = _dayNumberFormat.format(day);
+
+  return _RoutingPaneItem(
+    routePath: '/aoc$year/day$day',
+    icon: const Icon(FluentIcons.issue_solid),
+    title: Text('Day $dayString'),
+    routeBody: ChallengeSolverView(title: "AoC $year - Day $dayString", solver: solver),
+  );
+}
+
+_RoutingPaneItem get _overlayEffectsItem {
+  return _RoutingPaneItem(
+    routePath: '/overlayeffects',
+    icon: const Icon(FluentIcons.code),
+    title: const Text('Overlay effects'),
+    routeBody: const Center(child: OverlayEffects()),
+  );
+}
+
+class _RoutingPaneItemExpander extends PaneItemExpander {
+
+  final String routePath;
+  final Widget routeBody;
+
+  _RoutingPaneItemExpander({
+    required super.icon,
+    required super.title,
+    required super.items,
+    required this.routePath,
+    required this.routeBody,
+  }) : super(
+          key: ValueKey(routePath),
+          body: const SizedBox.shrink(),
+          onTap: () => _router.go(routePath),
+        );
+
+  GoRoute get goRoute => GoRoute(
+        path: routePath,
+        builder: (context, state) => routeBody,
+      );
+
+  List<GoRoute> get itemGoRoutes => items.map((item) => switch (item) {
+    _RoutingPaneItem() => [item.goRoute],
+    _RoutingPaneItemExpander() => [item.goRoute, ...item.itemGoRoutes],
+    _ => throw Exception('Cannot convert unsupported type <${item.runtimeType}> to GoRoute')
+  }).flattened.toList();
+
+}
+
+class _RoutingPaneItem extends PaneItem {
+
+  final String routePath;
+  final Widget routeBody;
+
+  _RoutingPaneItem({
+    required super.icon,
+    required super.title,
+    required this.routePath,
+    required this.routeBody,
+  }) : super(
+          key: ValueKey(routePath),
+          body: const SizedBox.shrink(),
+          onTap: () => _router.go(routePath),
+        );
+
+  GoRoute get goRoute => GoRoute(
+        path: routePath,
+        builder: (context, state) => routeBody,
+      );
+
+}
