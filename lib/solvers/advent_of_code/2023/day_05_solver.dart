775103e6c4973deabde2efaa80f83e9214aa446e
Sander in 't Hout <sander@inthout.me>
2023-12-06 00:17:50 +0100

    Optimize AoC 2023 day 5

--- lib/solvers/advent_of_code/2023/day_05_solver.dart
+++ lib/solvers/advent_of_code/2023/day_05_solver.dart
@@ -1,4 +1,3 @@
-import 'package:collection/collection.dart';
 import 'package:h3x_devtools/solvers/advent_of_code/2023/aoc_2023_solver.dart';
 import 'package:h3x_devtools/solvers/extensions.dart';
 
@@ -13,7 +12,7 @@ class Day05Solver extends AdventOfCode2023Solver {
   String getSolution(String input) {
     List<String> inputLineSets = input.split('\n\n');
     var inputData = (
-      seeds: inputLineSets[0].substring(7).split(' ').map(int.parse),
+      seeds: inputLineSets[0].substring(7).split(' ').map(int.parse).toList(),
       seedToSoilMap: _lineSetToMap(inputLineSets[1]),
       soilToFertilizerMap: _lineSetToMap(inputLineSets[2]),
       fertilizerToWaterMap: _lineSetToMap(inputLineSets[3]),
@@ -49,33 +48,32 @@ class Day05Solver extends AdventOfCode2023Solver {
       ),
     );
 
-    // Part 2
-    int part2 = inputData.seeds.slices(2).expand((element) => List.generate(element[1], (e) => element[0] + e, growable: false)).minBy(
-      (seed) {
-        return _resolveSourceToDestination(
-          _resolveSourceToDestination(
-            _resolveSourceToDestination(
-              _resolveSourceToDestination(
-                _resolveSourceToDestination(
-                  _resolveSourceToDestination(
-                    _resolveSourceToDestination(
-                      seed,
-                      inputData.seedToSoilMap,
-                    ),
-                    inputData.soilToFertilizerMap,
-                  ),
-                  inputData.fertilizerToWaterMap,
-                ),
-                inputData.waterToLightMap,
-              ),
-              inputData.lightToTemperatureMap,
-            ),
-            inputData.temperatureToHumidityMap,
-          ),
-          inputData.humidityToLocationMap,
-        );
-      },
-    );
+    // Part 2, TODO: This is faster than my original approach, but still takes about 1m30s
+    // Should be possible to optimize to a few ms.
+    int? part2;
+    for (int i = 0; part2 == null; i++) {
+      int x = _resolveDestinationToSource(i, inputData.humidityToLocationMap);
+      x = _resolveDestinationToSource(x, inputData.temperatureToHumidityMap);
+      x = _resolveDestinationToSource(x, inputData.lightToTemperatureMap);
+      x = _resolveDestinationToSource(x, inputData.waterToLightMap);
+      x = _resolveDestinationToSource(x, inputData.fertilizerToWaterMap);
+      x = _resolveDestinationToSource(x, inputData.soilToFertilizerMap);
+      x = _resolveDestinationToSource(x, inputData.seedToSoilMap);
+
+      bool doBreak = false;
+
+      for (int j = 0; j < inputData.seeds.length; j += 2) {
+        int seedMin = inputData.seeds[j];
+        int seedMax = inputData.seeds[j] + inputData.seeds[j + 1] - 1;
+        if (x >= seedMin && x <= seedMax) {
+          part2 = i;
+          doBreak = true;
+          break;
+        }
+      }
+
+      if (doBreak) break;
+    }
 
     return 'Part 1: $part1\nPart 2: $part2';
   }
@@ -94,6 +92,15 @@ class Day05Solver extends AdventOfCode2023Solver {
       }
     }
     return source;
+  }
+
+  int _resolveDestinationToSource(int destination, List<ObjectMap> map) {
+    for (var mapLines in map) {
+      if (destination >= mapLines.destination && destination < (mapLines.destination + mapLines.length)) {
+        return destination - mapLines.destination + mapLines.source;
+      }
+    }
+    return destination;
   }
 
 }
