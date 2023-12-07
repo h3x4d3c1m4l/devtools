e46e192df0841d72b38adeddbee051862b7ab73b
Sander in 't Hout <sander@inthout.me>
2023-12-07 09:55:27 +0100

    Add AoC 2023 day 07 solver Additionally add button for loading the sample input

--- lib/solvers/advent_of_code/aoc_solver.dart
+++ lib/solvers/advent_of_code/aoc_solver.dart
@@ -1,3 +1,8 @@
+// ignore_for_file: avoid_dynamic_calls
+
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
 import 'package:h3x_devtools/solvers/solver.dart';
 import 'package:h3x_devtools/storage.dart';
 import 'package:http/http.dart' as http;
@@ -30,6 +35,17 @@ abstract class AdventOfCodeSolver extends Solver<String, String> {
     await storeAdventOfCodeInput(yearNumber, dayNumber, httpResponse);
 
     return httpResponse;
+  }
+
+  Future<String> getSampleInput() async {
+    // Load list of assets
+    final manifestContent = await rootBundle.loadString('AssetManifest.json');
+    final Map<String, dynamic> manifestMap = json.decode(manifestContent);
+
+    // Find and read the code file asset
+    String sampleInputFilename = '${yearNumber}_${dayNumber.toString().padLeft(2, '0')}_sample.txt';
+    String sampleInputAssetPath = manifestMap.entries.where((entry) => entry.key.contains(sampleInputFilename)).single.value.first;
+    return await rootBundle.loadString(sampleInputAssetPath);
   }
 
 }
